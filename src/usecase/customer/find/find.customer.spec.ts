9dc5cd5883f1078c4ccbd45f7fcda6925e08e72d
ClaudiaThalita <claudiathalitamendes@outlook.com>
2023-02-07 23:28:43 -0300

    Entendendo problemas com testes de integração

--- src/usecase/customer/find/find.customer.spec.ts
+++ src/usecase/customer/find/find.customer.spec.ts
@@ -3,6 +3,7 @@ import Customer from "../../../domain/customer/entity/customer";
 import Address from "../../../domain/customer/value-object/address";
 import CustomerModel from "../../../infrastructure/customer/repository/sequelize/customer.model";
 import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
+import FindCustomerUseCase from "./find.customer.usecase";
 
 describe("Test find customer use case", () =>{
 
@@ -29,14 +30,26 @@ describe("Test find customer use case", () =>{
         const usecase = new FindCustomerUseCase(customerReposity)
 
         const costumer = new Customer("123","John");
-        const adress = new Address("Street",123, "city", "Zip");
-        costumer.changeAddress(adress);
+        const address = new Address("Street",123, "Zip","City");
+        costumer.changeAddress(address);
         await customerReposity.create(costumer);
 
         const input = {
             id:"123",
         }
 
-        const output = usecase.execute(input);
+        const output = {
+            id:"123",
+            name:"John",
+            address:{
+                street: "street",
+                city: "City",
+                number: 123,
+                zip: "Zip",
+            }
+        }
+        const result = usecase.execute(input);
+
+        expect(result).toEqual(output);
     });
 });
