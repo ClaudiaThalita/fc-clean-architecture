bceab33e7297323c7dc98ced1e83eeb621aa6a05
ClaudiaThalita <claudiathalitamendes@outlook.com>
2023-02-16 00:50:57 -0300

    Implementando testes de criar customer quando temos dados inválidos

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -38,4 +38,16 @@ describe("Unit test create customer use case", ()=>{
             },
         });
     });
+
+    it("should thwown an error when name is missing", async() => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        input.name = "";
+
+        await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+            "Name is required"
+        );
+        
+    })
 });
